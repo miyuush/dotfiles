1832d7a554ad26af14ff7a5066b48d49ccf223d5
miyuush <shiyuroad57@gmail.com>
2022-01-03 00:36:24 +0900

    Add fish setting

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# path
+set -x PATH /usr/local/go/bin $PATH
+set -x PATH $GOPATH/bin $PATH
+
+# ghq + fzf
+function ghq_fzf_repo -d 'Repository search'
+  ghq list --full-path | fzf --reverse --height=100% | read select
+  [ -n "$select" ]; and cd "$select"
+  echo " $select "
+  commandline -f repaint
+end
+
+# fish key bindings
+function fish_user_key_bindings
+  bind \cg ghq_fzf_repo
+end
+
+starship init fish | source
