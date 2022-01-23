cbde1f32766380fe679e0ff1be8f821bc4b2fa31
miyuush <shiyuroad57@gmail.com>
2022-01-24 02:10:08 +0900

    Update install.sh

--- workflow/install.sh
+++ workflow/install.sh
@@ -1,4 +1,15 @@
-echo "Install Packages"
+echo "Install fish"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install -y fish neovim tmux
+sudo apt install -y fish
+
+echo "Install starship"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+echo "Install etc"
+sudo apt install -y neovim tmux
+
+echo "Install Golang"
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install -y golang-go
