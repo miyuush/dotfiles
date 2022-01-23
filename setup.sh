5f059e7b26ddd384228d7c8163ff3508bbe2dcd3
miyuush <shiyuroad57@gmail.com>
2022-01-24 01:55:45 +0900

    Add setup scripts

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/miyuush/dotfiles.git
+cd ~/dotfiles
+. ~/dotfiles/workflow/install.sh
---
+++ workflow/install.sh
@@ -0,0 +1,4 @@
+echo "Install Packages"
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install -y fish neovim tmux
