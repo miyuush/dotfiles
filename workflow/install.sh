2db926bd109d41471074af5d3e2a6d47191d427e
miyuush <shiyuroad57@gmail.com>
2022-01-24 23:36:36 +0900

    Update workflow

--- workflow/install.sh
+++ workflow/install.sh
@@ -1,15 +1,27 @@
-echo "Install fish"
+echo "✅Install fish"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install -y fish
 
-echo "Install starship"
+echo "✅Install fisher"
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+echo "✅Install starship"
 bash <(curl -fsSL https://starship.rs/install.sh) --yes
 
-echo "Install etc"
+echo "✅Install etc"
 sudo apt install -y neovim tmux
 
-echo "Install Golang"
+echo "✅Install golang"
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt install -y golang-go
+
+echo "✅ Set config files"
+ln -sf $PWD/.config/ ~
+ln -sf $PWD/.tmux/ ~
+ln -sf $PWD/.bash_aliases ~/.bash_aliases
+ln -sf $PWD/.bash_profile ~/.bash_profile
+ln -sf $PWD/.bashrc ~/.bashrc
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
