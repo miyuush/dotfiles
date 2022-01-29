f4b3522917605d231eb44227c5c5148fa4ed33c4
miyuush <shiyuroad57@gmail.com>
2022-01-30 01:44:49 +0900

    Add kubernetes package

--- install_scripts/nvm.sh
+++ install_scripts/nvm.sh
@@ -1,4 +1,4 @@
 # see: https://docs.microsoft.com/ja-jp/windows/dev-environment/javascript/nodejs-on-wsl
 /bin/bash -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh)"
-exec $SHELL -l
+export NVM_DIR="$HOME/.nvm"
 nvm install --lts
