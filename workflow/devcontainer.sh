e9f7bf8153e2ad704b26f7a6ac3fc863a6defad3
miyuush <shiyuroad57@gmail.com>
2022-08-03 14:41:30 +0900

    Add workflow for devcontainer

---
+++ workflow/devcontainer.sh
@@ -0,0 +1,17 @@
+echo "✅ Set config files"
+. ./install_scripts/config_files.sh
+
+echo "✅ Install Homebrew"
+. ./install_scripts/homebrew.sh
+
+echo "✅ Set fzf key bindings"
+. ./install_scripts/fzf_key_binds.sh
+
+echo "✅ Install Vim-Plug"
+. ./install_scripts/vim-plug.sh
+
+echo "✅ Install Node.js"
+. ./install_scripts/nvm.sh
+
+echo "✅ Test Configuration"
+. ./workflow/test.sh
