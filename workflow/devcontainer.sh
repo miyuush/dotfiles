78f2300c218e9b367c078892a5b6eefac50cd548
miyuush <shiyuroad57@gmail.com>
2022-08-03 16:38:48 +0900

    Update devcontainer script

--- workflow/devcontainer.sh
+++ workflow/devcontainer.sh
@@ -3,8 +3,8 @@ cd ~/dotfiles
 echo "✅ Set config files"
 rm -rf ~/.config
 mkdir -p ~/.config/fish
-ln -sf $(pwd)/.devcontainer/config.fish/ ~/.config/fish/
-ln -sf $(pwd)/.devcontainer/abbreviations.fish/ ~/.config/fish/
+ln -sf $(pwd)/.devcontainer/config.fish ~/.config/fish/config.fish
+ln -sf $(pwd)/.devcontainer/abbreviations.fish ~/.config/fish/abbreviations.fish
 ln -sf $(pwd)/.tmux/ ~
 ln -sf $(pwd)/.bash_aliases ~/.bash_aliases
 ln -sf $(pwd)/.profile ~/.profile
